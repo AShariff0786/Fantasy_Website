de2a605f7c03ea38f09f30187a53841ea793e16f
Torendra Rasik <torendrarasik@gmail.com>
2021-01-19 16:45:32 -0500

    API connection
    Loaded in the api via balldontlie.io and returned the json format to nba endpoint

--- server/routes/nba.js
+++ server/routes/nba.js
@@ -1,10 +1,18 @@
 const express = require('express');
+const axios = require('axios');
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.json({
-        message: 'In nba router'
-    });
+const API_URL = 'https://www.balldontlie.io/api/v1/players';
+
+router.get('/', async (req, res) => {
+    try {
+        const { data } = await axios.get(API_URL);
+        res.json(data);
+    } catch (err) {
+        res.json({
+            message: "Our hamsters are busy at the moment."
+        })
+    }
 });
 
 module.exports = router;
