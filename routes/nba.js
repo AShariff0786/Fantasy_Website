9d6ff310b99903d99a8d3b0e65a945e6b880b2a8
Torendra Rasik <torendrarasik@gmail.com>
2021-01-20 17:22:47 -0500

    NBA Schedule Added
    - Fixed the working directory so server now sends static files through the public directory.
    
    - NBA route now automatically makes a GET request to the API and pulls the schedule for the current date
    
    - "Bootstrapified" the data and home page to make it look cleaner

---
+++ routes/nba.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const axios = require('axios');
+const router = express.Router();
+
+const API_URL = 'https://www.balldontlie.io/api/v1/';
+
+function getDate() {
+    var d = new Date(),
+        month = '' + (d.getMonth() + 1),
+        day = '' + d.getDate(),
+        year = d.getFullYear();
+
+    if (month.length < 2) 
+        month = '0' + month;
+    if (day.length < 2) 
+        day = '0' + day;
+
+    return [year, month, day].join('-');
+}
+
+router.get('/', async (req, res, next) => {
+    var date = getDate();
+    gamesURL = API_URL + `games?dates[]=${date}`;
+    let gamesData;
+    try {
+        const { data } = await axios.get(gamesURL);
+        gamesData = data.data;
+    } catch (err) {
+        next(err);
+    }
+    res.render('nba.ejs', {games: gamesData});
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -1,17 +1,23 @@
 const express = require('express');
 const nbaRouter = require('./routes/nba');
-require('dotenv').config()
+require('dotenv').config();
+
+const middlewares = require('./middlewares');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
+app.use(express.static(__dirname + '/public'));
 app.use('/nba', nbaRouter);
 
 app.get('/', (req, res) => {
-    res.render('../client/views/index.ejs');
+    res.render('index.ejs');
 });
 
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Listening at http://localhost:${PORT}`);
 })
